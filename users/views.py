8dffd7786215e0a5f1ffc6b236fc02e75b26b88c
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2023-04-18 16:57:02 -0300

    Estilização do login e cadastro

--- users/views.py
+++ users/views.py
@@ -7,19 +7,21 @@ from django.contrib.auth import logout as logout_django
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-# Create your views here.
-
+# Função para cadastrar usuario:
 def cadastrar(request):
     if request.method == 'POST':
+        # Pegando os valores dados pelo html "cadastrar.html":
         username = request.POST['username']
         email = request.POST['email']
         senha = request.POST['senha']
         
+        # Verificando se o usuario já existe:
         user = User.objects.filter(username=username).first()
-
+        
         if user:
             return HttpResponse('Já existe um usuário com esse nome')
         
+        # Criando um usuario:
         user = User.objects.create_user(username=username, email=email, password=senha)
         user.save()
 
@@ -27,23 +29,27 @@ def cadastrar(request):
 
     return render(request, 'users/cadastrar.html')
 
+# Função para efetuar o login do usuario:
 def login(request):
     if request.method == 'POST':
+        # Pegando os valores dados pelo html "login.html":
         username = request.POST['username']
         senha = request.POST['senha']
 
+        # Verificando o usuario:
         user = authenticate(username=username, password=senha)
 
         if user:
-            login_django(request, user)
+            login_django(request, user) # Logando o usuario
             return HttpResponseRedirect(reverse('tasks:index'))
 
         else:
             return HttpResponse('Nome ou senha invalidos!')
     return render(request, 'users/login.html')
 
+# Função para deslogar o usuario:
 def logout(request):
-    logout_django(request)
+    logout_django(request) # Deslogando o usuario
     return render(request, 'users/login.html', {
         'message': 'Você se desconectou'
     })
